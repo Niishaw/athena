e14f2a37d75cc16ed947a4c719ee9b06f191fa41
Niishaw <niishaw@gmail.com>
2022-11-02 21:23:03 +0200

    Card Page Phase 1

--- client/src/pages/creditcard/index.tsx
+++ client/src/pages/creditcard/index.tsx
@@ -1,13 +1,205 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography";
+import Cards from "react-credit-cards";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select, {SelectChangeEvent} from "@mui/material/Select";
+import Button from "@mui/material/Button";
+import "react-credit-cards/es/styles-compiled.css";
+import { countries } from "./config";
 
 const Details = () => {
 
+	const iCreditCard = {
+		number: "",
+		name: "",
+		expiry: "",
+		cvc: "",
+		country: {name: "", code: ""}
+	}
+
+	const [cardDetails, setCardDetails] = useState<any>(iCreditCard);
+	const [focus, setFocus] = useState<any>("");
+	const [month, setMonth] = useState<any>("");
+	const [expiry, setExpiry] = useState<any>("");
+
+	useEffect(() => {
+		setCardDetails({...cardDetails, expiry: month.concat(expiry)})
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [expiry, month])
+
+	const handleSetCountry = (evt, id) => {
+		console.log(id.props.id)
+		setCardDetails({...cardDetails, country: {name: evt.target.value, code: id.props.id}});
+	}
+
+	const setCardNumber = (evt) => {
+		setCardDetails({...cardDetails, number: evt.target.value});
+	}
+
+	const setCardName = (evt) => {
+		setCardDetails({...cardDetails, name: evt.target.value});
+	}
+
+	const handleSetMonth = (evt: SelectChangeEvent) => {
+		setMonth(evt.target.value);
+	}
+
+	const handleSetExpiry = (evt: SelectChangeEvent) => {
+		setExpiry(evt.target.value);
+	}
+
+	const setCVCNumber = (evt) => {
+		setCardDetails({...cardDetails, cvc: evt.target.value})
+	}
+
+	console.log(cardDetails)
+
     return (
         <Container maxWidth="lg" sx={{mt:15}}>
-            <Typography>Welcome to Details</Typography>
+           <Grid container spacing={1}>
+							<Grid item xs={12}>
+								<Cards 
+									number={cardDetails.number}
+									name={cardDetails.name}
+									expiry={cardDetails.expiry}
+									cvc={cardDetails.cvc}
+									focused={focus}
+								/>
+							</Grid>
+							<Container maxWidth="sm">
+							<Box className="col-sm-11">
+								<Grid container spacing={1}>
+									<Grid item xs={12}>
+										<Typography variant="subtitle1" sx={{mt: 2}}>Select Country</Typography>
+									</Grid>
+								</Grid>
+								<Grid container spacing={1}>
+									<Grid item xs={12} sx={{mt: 2}}>
+										<FormControl fullWidth size="small">
+											<InputLabel id="month-select">Country</InputLabel>
+											<Select
+												label="Country"
+												onChange={handleSetCountry}
+												name="country"
+												value={cardDetails.country.name}
+												onFocus={(evt) => setFocus(evt.target.name)}
+											>
+												<MenuItem value="">
+													<em></em>
+												</MenuItem>
+												{countries.map((country) => (
+													<MenuItem key={country.code} id={country.code} value={country.name}>{country.name}</MenuItem>
+												))}
+											</Select>
+										</FormControl>
+									</Grid>
+								</Grid>
+						 </Box>
+							<Box className="col-sm-11">
+								<Typography variant="subtitle1" sx={{mt: 2}}>Card Number</Typography>
+								<TextField 
+									size="small"
+									fullWidth
+									type="tel"
+									name="number"
+									onChange={setCardNumber}
+									inputProps={{maxLength: 16}}
+									onFocus={(evt) => setFocus(evt.target.name)}
+								/>
+						 </Box>
+						 <Box className="col-sm-11">
+								<Typography variant="subtitle1" sx={{mt: 2}}>Card Name</Typography>
+								<TextField 
+									size="small"
+									fullWidth
+									type="tel"
+									name="name"
+									onChange={setCardName}
+									onFocus={(evt) => setFocus(evt.target.name)}
+								/>
+						 </Box>
+						 <Box className="col-sm-11">
+								<Grid container spacing={1}>
+									<Grid item xs={4}>
+										<Typography variant="subtitle1" sx={{mt: 2}}>Expiration Date</Typography>
+									</Grid>
+								</Grid>
+								<Grid container spacing={1}>
+									<Grid item xs={4} sx={{mt: 2}}>
+										<FormControl sx={{ minWidth: 120}} size="small">
+											<InputLabel id="month-select">Month</InputLabel>
+											<Select
+												label="Month"
+												onChange={handleSetMonth}
+												name="month"
+												onFocus={(evt) => setFocus(evt.target.name)}
+											>
+												<MenuItem value="">
+													<em></em>
+												</MenuItem>
+												<MenuItem value="01">January</MenuItem>
+												<MenuItem value="02">February</MenuItem>
+												<MenuItem value="03">March</MenuItem>
+												<MenuItem value="04">April</MenuItem>
+												<MenuItem value="05">May</MenuItem>
+												<MenuItem value="06">June</MenuItem>
+												<MenuItem value="07">July</MenuItem>
+												<MenuItem value="08">August</MenuItem>
+												<MenuItem value="09">September</MenuItem>
+												<MenuItem value="10">October</MenuItem>
+												<MenuItem value="11">November</MenuItem>
+												<MenuItem value="12">December</MenuItem>
+											</Select>
+										</FormControl>
+									</Grid>
+									<Grid item xs={4} sx={{mt: 2}}>
+									<FormControl sx={{ minWidth: 120}} size="small">
+											<InputLabel id="year-select">Year</InputLabel>
+											<Select
+												label="Year"
+												name="year"
+												onChange={handleSetExpiry}
+												onFocus={(evt) => setFocus(evt.target.name)}
+											>
+												<MenuItem value="">
+													<em></em>
+												</MenuItem>
+												<MenuItem value="22">2022</MenuItem>
+												<MenuItem value="23">2023</MenuItem>
+												<MenuItem value="24">2024</MenuItem>
+												<MenuItem value="25">2025</MenuItem>
+												<MenuItem value="26">2026</MenuItem>
+												<MenuItem value="27">2027</MenuItem>
+												<MenuItem value="28">2028</MenuItem>
+												<MenuItem value="29">2029</MenuItem>
+												<MenuItem value="30">2030</MenuItem>
+											</Select>
+										</FormControl>
+									</Grid>
+									<Grid item xs={4} sx={{mt: 2}}>
+										<TextField 
+											size="small"
+											type="password"
+											label="CVV"
+											fullWidth
+											name="cvc"
+											onChange={setCVCNumber}
+											inputProps={{maxLength: 3}}
+											onFocus={(evt) => setFocus(evt.target.name)}
+										/>
+									</Grid>
+								</Grid>
+						 </Box>
+						 <Button fullWidth variant="contained" sx={{mt: 5}}>Add Card</Button>
+						</Container>
+					 </Grid>
         </Container>
     )
 }
--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,7 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
-import Typography from '../components/typography';
-import {AppBar, Toolbar} from '../components/appBar';
-import Grid from 'components/grid';
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import Typography from '@mui/material/Typography';
+import {AppBar, Toolbar} from '@mui/material';
+import Grid from '@mui/material/Grid';
+import IconButton from "@mui/material/IconButton"
+import ArrowBack from "@mui/icons-material/ArrowBack"
 
 import config from "./config";
 
@@ -10,10 +12,18 @@ import CreditCards from 'pages/creditcard';
 
 const AthenaRouter = () => {
 
+	const navigate = useNavigate()
+	const handleBackClick = () => {
+		navigate("/")
+	}
+
     return (
         <>
            <AppBar position="fixed" sx={(theme) => ({zIndex: theme.zIndex.drawer + 1, transition: theme.transitions.create(["width", "margin"], { easing: theme.transitions.easing.sharp, duration: theme.transitions.duration.leavingScreen})})} color="primary">
                 <Toolbar>
+									<IconButton edge="start" color="inherit" onClick={handleBackClick}>
+										<ArrowBack  />
+									</IconButton>
                     <Grid container spacing={1}>
                         <Grid item xs={12}>
                             <Typography variant='h6' textAlign="center" noWrap sx={(theme) => ({ flexGrow: 1, display: 'none', [theme.breakpoints.up('sm')]: {display: 'block'}})}>
